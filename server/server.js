fd396d3afc1824fed601abfe2c80dea223e4c11e
Austin Im <imaustinim@gmail.com>
2021-03-09 01:03:58 -0500

    new

--- server/server.js
+++ server/server.js
@@ -1,5 +1,36 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+// const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
+
+const indexRouter = require("../client/routes/index.routes");
+const usersRouter = require("../client/routes/users.routes");
+const projectsRouter = require("../client/routes/projects.routes");
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/projects", projectsRouter);
+
+
 // const path = require('path');
 
 // var createError = require('http-errors');
@@ -9,12 +40,6 @@
 // const indexRouter = require("./routes/index");
 // const usersRouter = require('./routes/users');
 
-// const port = 5000;
-// app.listen(5000, () => {
-//   console.log(`Server connected on port ${port}`);
-// })
-
-// // view engine setup
 // // app.set('views', path.join(__dirname, 'views'));
 // // app.set('view engine', 'ejs');
 
@@ -24,8 +49,7 @@
 // // app.use(cookieParser());
 // // app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
