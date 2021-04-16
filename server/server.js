4df61043fe6f4903d48d4a756b72c23bb70c39fe
Austin Im <imaustinim@gmail.com>
2021-04-16 15:21:54 -0400

    MERN Boilerplat split

--- server/server.js
+++ server/server.js
@@ -1,70 +1,24 @@
 const express = require('express');
-// const cors = require("cors");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
+const cors = require("cors");
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(__dirname)
+app.use(favicon(path.join(__dirname, "..", "client", 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "..", "client", 'build')));
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("MongoDB database connection established successfully");
-})
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+  });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
-
-const indexRouter = require("../client/routes/index.routes");
-const usersRouter = require("../client/routes/users.routes");
-const projectsRouter = require("../client/routes/projects.routes");
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/projects", projectsRouter);
-
-
-// const path = require('path');
-
-// var createError = require('http-errors');
-// // var cookieParser = require('cookie-parser');
-// // var logger = require('morgan');
-
-// const indexRouter = require("./routes/index");
-// const usersRouter = require('./routes/users');
-
-// // app.set('views', path.join(__dirname, 'views'));
-// // app.set('view engine', 'ejs');
-
-// // app.use(logger('dev'));
-// // app.use(express.json());
-// // app.use(express.urlencoded({ extended: false }));
-// // app.use(cookieParser());
-// // app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "client", 'build', 'index.html'));
+  });
